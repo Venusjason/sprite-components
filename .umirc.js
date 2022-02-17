5832e50fffeef3dfb569e0b3b92e7e01f28098d4
金麦郎 <jinxingfe@gmail.com>
2022-02-17 22:35:16 +0800

    feat: dist

--- .umirc.js
+++ .umirc.js
@@ -30,6 +30,8 @@ const isDeploy = process.env.SITE_DEPLOY === 'TRUE';
 
 export default {
   title: 'a-sprite',
+  base: 'https://venusjason.github.io/a-sprite-doc-site/',
+  publicPath: 'https://venusjason.github.io/a-sprite-doc-site/',
   mode: 'site',
   logo: 'https://s4.aconvert.com/convert/p3r68-cdx67/axyt7-0km7f.svg',
   extraBabelPlugins: [
